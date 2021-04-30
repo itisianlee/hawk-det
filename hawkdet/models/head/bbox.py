8385aa9cac40bb2c66a59639c7c69fcc98df23da
itisianlee <ianleedream@163.com>
2021-04-30 09:07:08 +0800

    bounding box head

---
+++ hawkdet/models/head/bbox.py
@@ -0,0 +1,13 @@
+import torch.nn as nn
+
+
+class BboxHead(nn.Module):
+    def __init__(self, inchannels=512, num_anchors=3):
+        super().__init__()
+        self.conv1x1 = nn.Conv2d(inchannels, num_anchors*4, kernel_size=(1,1), stride=1, padding=0)
+
+    def forward(self,x):
+        out = self.conv1x1(x)
+        out = out.permute(0, 2, 3, 1).contiguous()
+
+        return out.view(out.shape[0], -1, 4)
