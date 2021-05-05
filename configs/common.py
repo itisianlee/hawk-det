7c09ace6b4ce51bd21df45631ef129b01beebae2
itisianlee <ianleedream@163.com>
2021-05-05 08:27:21 +0800

    newest config

--- configs/common.py
+++ configs/common.py
@@ -1,8 +1,8 @@
 from hawkdet.utils.yacs import CfgNode as CN
 
 C = CN()
-C.batch_size = 24
-C.max_epoch = 100
+C.batch_size = 32
+C.max_epoch = 250
 C.num_workers = 4
 C.lr = 1e-3
 C.weight_decay = 5e-4
@@ -15,29 +15,32 @@ C.loc_lambda = 2.0
 C.clip = False
 C.output_path = 'det-outputs'
 C.checkpoint_every = 5
-C.milestones = [30, 60, 90]  # epoch
+C.milestones = [190, 220]  # epoch
 
 C.Dataset = CN()
 C.Dataset.name = 'WiderFace'
-C.Dataset.image_size = (840, 840)
-C.Dataset.label_file = '/Users/lijianwei04/Desktop/widerface/widerface/train/label.txt'
+C.Dataset.image_size = (640, 640)
+C.Dataset.label_file = '/root/paddlejob/workspace/hawk-facedet/data/widerface/train/label.txt'
 C.Dataset.image_mean = (104, 117, 123)  # bgr order
+# C.Dataset.image_mean = [104, 111, 120]  # bgr order
+C.Dataset.image_std = [13.28474724, 12.9287223, 13.38387188]
 
 C.Detector = CN()
 C.Detector.name = 'retinaface'
 C.Detector.params = CN()
-C.Detector.params.backbone_return_layers = ['layer2', 'layer3', 'layer4']
+C.Detector.params.backbone_return_layers = ['stage1', 'stage2', 'stage3']
 C.Detector.Backbone = CN()
-C.Detector.Backbone.name = 'resnet50'
+C.Detector.Backbone.name = 'mobilenetv1_0x25'
 C.Detector.Backbone.params = CN()
-C.Detector.Backbone.params.pretrained = False
+C.Detector.Backbone.params.pretrained_model = 'mobilenetV1X0.25_pretrain.tar'
+# C.Detector.Backbone.params.pretrained = True
 C.Detector.Backbone.params.progress = True
 
 C.Detector.Stem = CN()
 C.Detector.Stem.name = 'fpn'
 C.Detector.Stem.params = CN()
-C.Detector.Stem.params.in_channels = 256
-C.Detector.Stem.params.out_channels = 256
+C.Detector.Stem.params.in_channels = 32
+C.Detector.Stem.params.out_channels = 64
 C.Detector.Stem.params.context_module = 'ssh'
 
 C.Detector.Head = CN()
