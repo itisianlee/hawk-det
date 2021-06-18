859d33a9033facead1914510a2420d9660fd8fa5
itisianlee <ianleedream@163.com>
2021-06-18 17:18:18 +0800

    update arg

--- hawkdet/loss/base_loss.py
+++ hawkdet/loss/base_loss.py
@@ -4,11 +4,11 @@ import torch.nn.functional as F
 
 
 class AnchorLoss(nn.Module):
-    def __init__(self, num_classes=2, overlap_threshold=0.3, pos_neg_ratio=1/3, variance=[0.1, 0.2]):
+    def __init__(self, num_classes=2, overlap_threshold=0.3, neg_pos_ratio=3, variance=[0.1, 0.2]):
         super().__init__()
         self.num_classes = num_classes
         self.overlap_threshold = overlap_threshold
-        self.pos_neg_ratio = pos_neg_ratio
+        self.neg_pos_ratio = neg_pos_ratio
         self.variance = variance
 
     def match(self):
