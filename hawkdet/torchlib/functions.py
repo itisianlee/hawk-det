939cf68818fecf9dee9848319393aaf029ead2ae
itisianlee <ianleedream@163.com>
2021-06-23 22:22:24 +0800

    update: log sum exp

---
+++ hawkdet/torchlib/functions.py
@@ -0,0 +1,12 @@
+import torch
+
+
+def log_sum_exp(x):
+    """Utility function for computing log_sum_exp while determining
+    This will be used to determine unaveraged confidence loss across
+    all examples in a batch.
+    Args:
+        x (Variable(tensor)): conf_preds from conf layers
+    """
+    x_max = x.data.max()
+    return torch.log(torch.sum(torch.exp(x-x_max), 1, keepdim=True)) + x_max
